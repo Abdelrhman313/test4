579dfca683d78029b24b5e58d494a1992323a2fc
Abdelrhman <58594875+Abdelrhman313@users.noreply.github.com>
2020-04-16 19:41:08 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset='utf-8' />
+    <meta name="viewport" content="width=device-width, initial-Scale = 1"/>
+    <title>Sass Website</title>
+    <link rel="stylesheet" href="Css/homepage.css" />
+    <link rel="stylesheet" href="Css/normalize.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Coda&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!--Start header-->
+    <div class="header">
+        <div class="container">
+            <div class="logo">
+                <h1>Composition</h1>
+                <p>Free PSD Website Templete</p>
+            </div>
+            <div class="collapse">
+                <i class="fa fa-bars"></i>
+                <i class="fa fa-times"></i>
+            </div>
+            <ul class="navbar">
+                <li><a href="#">HomePage</a></li>
+                <li><a href="#">Style Demo</a></li>
+                <li><a href="#">Full Width</a></li>
+                <li><a href="#">Dropdown</a></li>
+                <li><a href="#">Link Text</a></li>
+            </ul>
+        </div>
+    </div>
+    <!--End header-->
+
+    <!--Start Intro-->
+    <div class="intro">
+        <div class="container">
+            <div class="heading">
+                <h2>Our Gallary</h2>
+            </div>
+            <div class="image-bullets">
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."</p>
+                <img src="images/img.jpg" alt="image">
+                <div class="bullets">
+                    <ul>
+                        <li class="selected"></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Intro-->
+
+    <!--start Services-->
+    <div class="services">
+        <div class="container">
+            <div class="heading">
+                <h2>Our Services</h2>
+            </div>
+            <div class="service center-all">
+                <h3><i class="fa fa-home"></i>Introduction Facilis</h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc Lorim ipsum ddeq vein kminc sjkni.</p>
+                <a href="#">Read More...</a>
+            </div>
+            <div class="service center-all">
+                <h3><i class="fa fa-users"></i>Introduction Facilis</h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc Lorim ipsum ddeq vein kminc sjkni.</p>
+                <a href="#">Read More...</a>
+            </div>
+            <div class="service center-all">
+                <h3><i class="fa fa-gears"></i>Introduction Facilis</h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc Lorim ipsum ddeq vein kminc sjkni.</p>
+                <a href="#">Read More...</a>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--End Services-->
+
+    <!--Start Player-->
+    <div class="players">
+        <div class="container">
+            <div class="heading">
+                <h2>Our Player</h2>
+            </div>
+            <div class="player center-all">
+                <img src="images/1.jpg" alt="image">
+                <h3>Name : <span>John Duo</span></h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni</p>
+                <a href="#">View Profile..</a>
+            </div>
+            <div class="player center-all">
+                <img src="images/2.jpg" alt="image">
+                <h3>Name : <span>John Duo</span></h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni</p>
+                <a href="#">View Profile..</a>
+            </div>
+            <div class="player center-all">
+                <img src="images/3.jpg" alt="image">
+                <h3>Name : <span>John Duo</span></h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni</p>
+                <a href="#">View Profile..</a>
+            </div>
+            <div class="player center-all">
+                <img src="images/4.jpg" alt="image">
+                <h3>Name : <span>John Duo</span></h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni</p>
+                <a href="#">View Profile..</a>
+            </div>
+            <div class="player center-all">
+                <img src="images/5.jpg" alt="image">
+                <h3>Name : <span>John Duo</span></h3>
+                <p>Lorim ipsum ddeq vein kminc sjkni Lorim ipsum ddeq vein kminc sjkni</p>
+                <a href="#">View Profile..</a>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--End Player-->
+
+    <!--Start Lorim Ipsum-->
+    <div class="lorim">
+        <div class="container">
+            <div class="heading">
+                <h2>Lorim Ipsum</h2>
+            </div>
+            <div class="lorim-img">
+                <div class="overlay">image 1</div>
+                <img src="images/01.jpg" alt="image">
+            </div>
+            <div class="lorim-img">
+                <div class="overlay">image 1</div>
+                <img src="images/02.jpg" alt="image">
+            </div>
+            <div class="lorim-img last">
+                <div class="overlay">image 1</div>
+                <img src="images/03.jpg" alt="image">
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--Start Lorim Ipsum-->
+
+    <!--Start Footer-->
+    <div class="footer">
+        <div class="container">
+            <div class="posts">
+                <h2>Our Posts</h2>
+                <div class="post">
+                    <img src="images/002.jpg">
+                    <div class="text">
+                        <h3>Last Post</h3>
+                        <p>Lorem Ipsum basff ani vasv wnoinaw Lorem Ipsum basff ani vasv wnoinaw Lorem Ipsum basff ani vasv wnoinaw Lorem Ipsum wnoinaw.</p>
+                    </div>
+                </div>
+                <div class="post">
+                    <img src="images/003.jpg">
+                    <div class="text">
+                        <h3>Last Post</h3>
+                        <p>Lorem Ipsum basff ani vasv wnoinaw Lorem Ipsum basff ani vasv wnoinaw Lorem Ipsum basff ani vasv wnoinaw Lorem Ipsum wnoinaw.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="company">
+                <h2>About Company</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>Gallary</li>
+                    <li>Services</li>
+                    <li>Player</li>
+                    <li>Phone</li>
+                    <li>Email: <span>Test-Email22@mail.com</span></li>
+                    <li>Address: <span>Tahma-Giza-Egypt</span></li>
+                </ul>
+                <h3>Lorim Ipsum</h3>
+                <p>Lorem ipsum nasc gresa polat</p>
+            </div>
+            <div class="contact">
+                <h2>Contact Us</h2>
+                <form>
+                    <input type="text" placeholder="User Name" />
+                    <input type="email" placeholder="E-mail" />
+                    <textarea>Message</textarea>
+                    <input type="submit" value="SUBMIT" />
+                </form>
+            </div>
+            <div class="clearfix"></div>
+            <div class="copyright">
+                <div class="copy">Al Right Reserved &copy; <span>2020</span></div>
+                <div class="auther">Template Designed By <span>BoDy</span></div>
+            </div>
+        </div>
+    </div>
+    <!--End Footer-->
+
+    <script src="Js/jquery.js"></script>
+    <script src="Js/plugins.js"></script>
+</body>
+
+</html>
